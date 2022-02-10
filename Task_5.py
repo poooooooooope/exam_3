71cb7b23d40f1f3a9b75ac262ac219dbc7f3b7cc
poooooooooope <93179316+poooooooooope@users.noreply.github.com>
2022-02-10 15:57:49 +0600

    Update Task_5.py

--- Task_5.py
+++ Task_5.py
@@ -1,4 +1,6 @@
 import csv
+import sys
+import pandas as pandasForSortingCSV
 
 
 class WorkWithCsvFile:
@@ -7,10 +9,16 @@ class WorkWithCsvFile:
         self.our_csv_file = our_csv_file
 
     def sort_by_first_field(self):
-        pass
-
-    def add_a_row(self):
-        pass
+        csvData = pandasForSortingCSV.read_csv(self.our_csv_file)
+        csvData.sort_values([1], 
+                    axis=0,
+                    ascending=[False], 
+                    inplace=True)
+        
+    def add_a_row(self, new_row):
+        addrow = self.our_csv_file
+        addrow.writerow(new_row)
+        
 
     def update_a_row(self):
         pass
@@ -18,8 +26,14 @@ class WorkWithCsvFile:
     def correct_a_row(self):
         pass
 
-    def search_elements_by_condition(self):
-        pass
+    def search_elements_by_condition(self, enter_number):
+        number = enter_number
+        csv_file = csv.reader(open(self.our_csv_file, "r"), delimiter=",")
+        
+        for row in csv_file:
+            if number == row[1]:
+         print (row)
+        
 
 file_name = ''
 
